41ca73f28152543a01ba280791cc16b915d6bfef
draggem <draggem0@icloud.com>
2020-08-27 18:39:09 +1200

    Email implemented but needs to plug in variables

--- MaoProduce-delivery-app/Functions/SendEmailFunctions.cs
+++ MaoProduce-delivery-app/Functions/SendEmailFunctions.cs
@@ -100,7 +100,7 @@ namespace MaoProduce_delivery_app.Functions
                             Mao Produce";
 
             // The HTML body of the email.
-            string htmlBody = @"";
+            string htmlBody = "<center> <table width=\"100%\" style=\"max-width:620px;margin:0 auto;border:1px solid black; background-color:#ffffff\" bgcolor=\"#ffffff\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\"> <tbody><tr> <td width=\"100%\" style=\"text-align:left\"> <table width=\"100%\" cellspacing=\"0\" border=\"0\" cellpadding=\"20\" bgcolor=\"#ffffff\" style=\"background-color:#ffffff\"> <tbody><tr> <td width=\"30\"> <center> <img width=\"170px\" height=\"auto\" style=\"width:170px;height:auto\" alt=\"Mao Produce\" src=\"https://shop.maoproduce.co.nz/image/catalog/logo.png\" data-image-whitelisted=\"\"> <h2 style=\"color:#3a9821; font-family: Arial, Helvetica, sans-serif; font-size:34px; font-weight: normal; line-height: 1.3;\">Dispatch Slip</h2> </center> </td> </tr> </tbody></table> </td> </tr> <tr> <td width=\"100%\" style=\"text-align:left\"> <table width=\"100%\" cellspacing=\"20\" border=\"0\" cellpadding=\"20\" bgcolor=\"#3a9821\" style=\"background-color:#ffffff\"> <tbody><tr> <td bgcolor=\"#ffffff\" style=\"background-color:#ffffff\"> <h2 style=\"font-family:Arial, Helvetica, sans-serif; line-height: 1.3; font-weight: normal; font-size:20px\">Hi, {{customer_name}}</h2> <p style=\"font-family:Arial, Helvetica, sans-serif; line-height: 1.3; font-weight: normal; font-size: 15px; color:#474747\">You will find the order details in this email. <br> If you have any questions according to your order or about, please let us know.<br> Thanks for choosing Mao Produce. <br> </p><table border=\"0\" cellspacing=\"0\" cellpadding=\"0\" width=\"100%\"> <tbody><tr> <td valign=\"top\" width=\"50%\"> <table border=\"0\" cellspacing=\"0\" cellpadding=\"3\"> <tbody><tr> <td valign=\"top\">Order Number:</td> <td valign=\"top\"><strong>{{orderId}}</strong></td> </tr> <tr> <td valign=\"top\">Signee:</td> <td valign=\"top\"> <strong>{{signee}}</strong> </td> <td> <img width=\"100px\" height=\"auto\" src=\"{{signatureUrl}}\" valign=\"bottom\" alt=\"signature\" align=\"center\" class=\"text-center\"> </td> </tr> <tr> <td>&nbsp;</td> </tr> </tbody></table> </td> <td valign=\"top\" width=\"50%\"> </td> </tr></tbody></table><hr> <div style=\"font-size:11px\"> <table style=\"width:100%\" cellspacing=\"0\" cellpadding=\"3\"> <thead> <tr> <th valign=\"top\" align=\"left\" style=\"background:#e3e3e3\">Description</th> <th valign=\"top\" align=\"right\" width=\"30\" style=\"background:#e3e3e3\">Qty</th> <th valign=\"top\" align=\"right\" width=\"100\" style=\"background:#e3e3e3\">Price</th> <th colspan=\"2\" valign=\"top\" align=\"right\" width=\"100\" style=\"background:#e3e3e3\">Sub-Total</th> </tr> </thead> <tbody> {{#each item}} <tr> <td valign=\"top\" align=\"left\">{{description}}</td> <td valign=\"top\" align=\"right\" width=\"30\">{{quantity}}</td> <td valign=\"top\" align=\"right\" width=\"100\">${{price}}</td> <td colspan=\"2\" valign=\"top\" align=\"right\" width=\"100\">${{subTotal}}</td> </tr> {{/each}} <tr> <td align=\"left\" colspan=\"5\" style=\"border-bottom:solid 1px #e3e3e3\"></td> </tr> </tbody> <tfoot> <tr> <td align=\"right\" colspan=\"4\" style=\"background:#e3e3e3\">Total:</td> <td align=\"right\" style=\"background:#e3e3e3\">${{totalPrice}}</td> </tr> </tfoot> </table> </div><p><em style=\"font-family: Arial, Helvetica, sans-serif; font-size: 12px; color:#777777\"> <br> <center> \u00A9 Copyright {{year}} Powered by <a style=\"color:#3a9821;\" href=\"https://canit.co.nz\">CanIT Limited.</a> All Rights Reserved.</p> </center></em></p> </td> </tr> </tbody></table> </td> </tr> </tbody></table> </center>";
 
             // Replace USWest2 with the AWS Region you're using for Amazon SES.
             // Acceptable values are EUWest1, USEast1, and USWest2.
@@ -120,41 +120,39 @@ namespace MaoProduce_delivery_app.Functions
                 new List<string> { "admin@maoproduce.co.nz" },
                 Content = new EmailContent
                 {
-                    //Trying to send the templated email from ses
-                    Template = new Template
-                    {
-                        TemplateData = "{\r\n    \"orderId\": \"123123123\",\r\n    \"customer_name\": \"Test 1\",\r\n    \"iteration_of_veggies\": \"Bock Bock Choy\",\r\n    \"veggie_price\": \"$2.00\",\r\n    \"quantity\": \"5\",\r\n    \"sub_total\": \"$10.00\",\r\n    \"total_price\": \"$10.00\"\r\n}",
-                        TemplateName = "Blue-Test"
-                    }
-
-                    //    Simple = new Message
-                    //    {
-                    //        Subject = new Content
-                    //        {
-                    //            Data = subject
-                    //        },
-                    //        Body = new Body
-                    //        {
-                    //            Html = new Content
-                    //            {
-                    //                Data = htmlBody
-                    //            },
-                    //            Text = new Content
-                    //            {
-                    //                Data = textBody
-                    //            }
-                    //        }
-                    //    }
+                    ////Trying to send the templated email from ses
+                    //Template = new Template
+                    //{
+                    //    TemplateData = "{}",
+                    //    TemplateName = "MaoProduce-Email-Template-4"
                     //},
-                    // If you are not using a configuration set, comment
-                    // or remove the following line 
-                    //ConfigurationSetName = configSet
+
+                        Simple = new Message
+                        {
+                            Subject = new Content
+                            {
+                                Data = subject
+                            },
+                            Body = new Body
+                            {
+                                Html = new Content
+                                {
+                                    Data = htmlBody
+                                },
+                                Text = new Content
+                                {
+                                    Data = textBody
+                                }
+                            }
+                        }
+
+                        // If you are not using a configuration set, comment
+                        // or remove the following line 
+                        //ConfigurationSetName = configSet
                 }
             };
             try
             {
-                var tempObj = await client.GetEmailTemplateAsync(new GetEmailTemplateRequest { TemplateName = "Blue-Test" });
-                context.Logger.LogLine(JsonConvert.SerializeObject(tempObj));
                 context.Logger.LogLine("Sending email using Amazon SES...");
                 var response = await client.SendEmailAsync(sendRequest);
                 if (response.HttpStatusCode == HttpStatusCode.OK){
